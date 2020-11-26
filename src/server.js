213145929fa1c8c4b86a505f6f3895fb0c5b18fb
Leo Janas <leojanas7@gmail.com>
2020-11-26 09:42:15 -0700

    Prepare for deployment

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
-const app = require('./app')
-const {PORT} = require('./config')
+const express = require('express');
+const app = require('./app');
+const {PORT} = require('./config');
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+ app.get('/api/*', (req, res) => {
+   res.json({ok: true});
+ });
+
+ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+ module.exports = {app};
