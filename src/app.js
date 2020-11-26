068135b29fd4c1b4de2dd08b8ca7c6953b857e2d
Leo Janas <leojanas7@gmail.com>
2020-11-26 09:49:07 -0700

    Deploy and add cors

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
+const {CLIENT_ORIGIN} = require('./config');
 
 const app = express()
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
