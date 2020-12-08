32048b09ae4c246924b340b24177701d147aa248
Leo Janas <leojanas7@gmail.com>
2020-12-08 11:24:05 -0700

    fix cors issue

--- src/app.js
+++ src/app.js
@@ -10,11 +10,6 @@ const recipeRouter = require('./recipes/recipe-router');
 const mealplanRouter = require('./mealplan/mealplan-router');
 
 const app = express()
-app.use(
-  cors({
-      origin: CLIENT_ORIGIN
-  })
-);
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -22,7 +17,11 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 
 app.use('/api/inventory', inventoryRouter)
 app.use('/api/recipes', recipeRouter)
